4a726151dc8432323e18e6cb954cc6b8cf1efaeb
root <root@ip-172-31-18-61.ec2.internal>
2026-07-10 21:04:57 +0000

    test

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,17 @@
+package com.example;
+
+public class App {
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello Maven");
+
+        int result = add(10,20);
+
+        System.out.println("Result = " + result);
+    }
+}
